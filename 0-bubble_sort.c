d062dbcb0d5b5e1c3b1cd36290de688e5bde22e1
jeremias <jeremiaserba7894@gmail.com>
2023-05-17 12:38:18 -0700

    Bubble sort - Fix errors by betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,22 +3,22 @@
 void
 bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 0, temp = 0;
+	size_t i = 0, j = 0, temp = 0;
 
-    if (!array || !size)
-        return;
+	if (!array || !size)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < (size - i - 1); j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
