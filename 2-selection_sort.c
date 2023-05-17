0c92d979de2b299fde88c0da74db064ccaf5d25f
jeremias <jeremiaserba7894@gmail.com>
2023-05-17 13:54:05 -0700

    Task 2 - Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,13 +18,10 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
-			{
 				min = j;
-			}
 		}
 		if (min != i)
 		{
