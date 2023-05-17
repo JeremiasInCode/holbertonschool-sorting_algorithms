383c37778f800e4f264ed8259c632da53ef3fa56
jeremias <jeremiaserba7894@gmail.com>
2023-05-17 12:26:10 -0700

    Bubble sort 1 - Add verification and developed do 0-O.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,16 @@
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size)
+void
+bubble_sort(int *array, size_t size)
 {
     size_t i = 0, j = 0, temp = 0;
 
-    for (i = 0; i < size; i++)
+    if (!array || !size)
+        return;
+
+    for (i = 0; i < size - 1; i++)
     {
-        for (j = 0; j < size; j++)
+        for (j = 0; j < (size - i - 1); j++)
         {
             if (array[j] > array[j + 1])
             {
